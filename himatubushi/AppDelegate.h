336b7758b08193f0e7600feeee27158529f96559
Syotaro Shimizu <syotaroshimizu@Syotaro-no-MacBook-Pro.local>
2014-12-25 01:07:21 +0900

    Initial Commit

---
+++ himatubushi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  himatubushi
+//
+//  Created by Syotaro Shimizu on 2014/12/25.
+//  Copyright (c) 2014年 Syotaro Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ himatubushi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  himatubushi
+//
+//  Created by Syotaro Shimizu on 2014/12/25.
+//  Copyright (c) 2014年 Syotaro Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
